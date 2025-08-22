0ba705e6913086e8a39a019b8cfdeca557eeb8df
Kauã Barros <kauabarrostrabalho@gmail.com>
2025-08-22 14:34:47 -0300

    fix: Correção da visualização de mensagem sobre a lista vazia e removido o disabled do botão sortear

--- app.js
+++ app.js
@@ -5,18 +5,20 @@ let amigo = document.getElementById("amigo");
 listaAmigos = document.getElementById("listaAmigos");
 titulo = document.getElementById("titulo");
 
-//Função para adicionar um amigo à array, ele não pode ser um nome vazio. 
+//Função para adicionar um amigo à array, ele não pode ser um nome vazio.
 // Além disso, exibe a lista de amigos cadastrados em formato de lista
 function adicionarAmigo() {
   if (amigo.value === "" || /[0-9]/.test(amigo.value)) {
-    alert("Por favor digite um nome válido, ele não pode conter numeros ou ser vazio!");
-
+    amigo.value = "";
+    alert(
+      "Por favor digite um nome válido, ele não pode conter numeros ou ser vazio!"
+    );
     return;
   }
   arrayAmigos.push(amigo.value);
   listaAmigos.innerHTML = arrayAmigos.join("<br>");
   amigo.value = "";
-  document.getElementById("sortearAmigo").disabled = false;
+  document.getElementById("sortearAmigo");
 }
 //função para sortear o amigo secreto, ela sorteia um número aleatório dentro do array e exibe o nome sorteado,
 //  ao final verifica se ainda há nomes diponiveis, removendo o nome sorteado e atualizando a lista.
@@ -24,14 +26,17 @@ function sortearAmigo() {
   let amigoSorteado = Math.floor(Math.random() * arrayAmigos.length);
   console.log(arrayAmigos.length);
   if (amigoSorteado === null || arrayAmigos.length === 0) {
-    document.getElementById("sortearAmigo").disabled = true;
-    listaAmigos.innerHTML = "Não há mais amigos para sortear, me desculpe :/";
+    document.getElementById("sortearAmigo");
   }
 
   arrayAmigos.length != 0
     ? (titulo.innerHTML = `o amigo sorteado é: ${arrayAmigos[amigoSorteado]}`)
-    : (titulo.innerHTML = `não há amigos cadastrados, por favor adicione um amigo!         `);
- 
+    : (titulo.innerHTML = `não há amigos cadastrados, por favor adicione um amigo!`);
+
+  arrayAmigos == 0
+    ? (listaAmigos.innerHTML =
+        "Não há mais amigos para sortear, me desculpe :/")
+    : (listaAmigos.innerHTML = arrayAmigos.join("<br>"));
+
   arrayAmigos.splice(amigoSorteado, 1);
-  listaAmigos.innerHTML = arrayAmigos.join("<br>");
 }
